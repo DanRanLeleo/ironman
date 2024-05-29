7468567f13eba185979a59b1bf7772eddfa85c1c
LEONARDO KAUÃ DANELI <1136110@ATITUS.LOCAL>
2024-05-29 19:46:57 -0300

    Versao inicial

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Opa")
+import os, pygame, time
+os.system("cls")
+tamanho = (800,600)
+relogio = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )
+pygame.display.set_caption("Iron Man do Marcão")
+branco = (255,255,255)
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+
+    tela.fill(branco)
+    pygame.display.update()
+    relogio.tick(60)
